c8b78f8c41bacb128ba7cb5f197ef14de64af01f
Luis PM <luisp237@gmail.com>
2020-03-27 13:08:13 -0400

    login post test completed

--- auth/auth.spec.js
+++ auth/auth.spec.js
@@ -7,16 +7,16 @@ describe('server.js endpoints', () => {
             const response = await request(server).get('/')
             expect(response.status).toBe(200);
         })
-    }
-    )
+    })
 
+    describe('POST /register', () => {
 
         it(' should return 201 if username & password in body', async () => {
             return request(server)
                 .post('/api/auth/register')
                 .send({ 
-                    "username": "kyle1",
-                    "password": "kyle1"
+                    "username": "kyle1000",
+                    "password": "kyle1000"
                 })
                 .expect(201);
         });
@@ -25,10 +25,48 @@ describe('server.js endpoints', () => {
             return request(server)
                 .post('/api/auth/register')
                 .send({ 
-                    "username": "kyle",                    
+                    "username": "kyle2",                    
                 })
                 .expect(500);
         })
+    })
+
+        describe('POST /login', () => {
+            it('should return JSON object', () => {
+                return request(server)
+                .post('/api/auth/login')
+                .send({
+                    "username": "kyle",
+                    "password": "kyle"
+                })
+                .then(res => {
+                     expect(res.type).toMatch(/json/i);
+                })
+               
+            })
+        
+
+            it(' should return 401 if username doesnt exist', async () => {
+                return request(server)
+                    .post('/api/auth/login')
+                    .send({ 
+                        "username": "notanuser",
+                        "password": "notapassword"
+                    })
+                    .expect(401);
+            });
+
+        // it.todo(' should return 500 if username & password NOT in body', async () => {
+        //     return request(server)
+        //         .post('/api/auth/register')
+        //         .send({ 
+        //             "username": "kyle2",                    
+        //         })
+        //         .expect(500);
+        // })
+
+        });
+
     });
     
 
